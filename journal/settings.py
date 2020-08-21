416f4d941cfc89e0deb9e3792ec9c2e2e5f2c37c
Ivan Toulou <touloumbadjiani@gmail.com>
2020-08-21 14:46:19 +0100

    prod settings

--- journal/settings.py
+++ journal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 # import environ
+import dj_database_url
 from pathlib import Path
 
 # env = environ.Env()
@@ -83,12 +84,16 @@ WSGI_APPLICATION = 'journal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
