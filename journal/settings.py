b4b8db54c92799e269183c4dc31a08bc5903bf31
Ivan Toulou <touloumbadjiani@gmail.com>
2020-08-21 14:32:34 +0100

    prod settings

--- journal/settings.py
+++ journal/settings.py
@@ -13,8 +13,8 @@ import os
 # import environ
 from pathlib import Path
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
