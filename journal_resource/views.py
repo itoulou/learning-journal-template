006b414ea2f8851c6f553100611751f003aae012
Ivan Toulou <touloumbadjiani@gmail.com>
2020-08-24 09:47:23 +0100

    download attachment

--- journal_resource/views.py
+++ journal_resource/views.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import boto3
-from pathlib import Path
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from create_resource.models import Resource
 from create_resource.forms import ResourceForm
-from journal.settings import AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 # Create your views here.
 def home(request):
@@ -25,7 +22,6 @@ def update_resource(request, pk):
             form.save()
             return redirect('home')
 
-
     else:
         form = ResourceForm(instance=resource)
         return render(request, 'update_resource.html', {'form': form})
@@ -36,11 +32,3 @@ def delete_resource(request, pk):
     resource.delete()
     return redirect('home')
 
-def download_attachment(request, pk):
-    print(dir(request.user))
-    print(Path.home())
-    resource = get_object_or_404(Resource, pk=pk)
-    s3 = boto3.resource('s3')
-    s3.Bucket(AWS_STORAGE_BUCKET_NAME).download_file('media/' + str(resource.attachment), '/home/sol-022/' + str(resource.filename()))
-    messages.success(request, 'File {} downloaded'.format(resource.filename()))
-    return redirect('home')
